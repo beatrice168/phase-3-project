8557e026c837fb16aaebedc85239773a260e2377
beatrice <helgapaul389@gmail.com>
2023-06-07 18:14:20 +0300

    second commit

--- lib/models.py
+++ lib/models.py
@@ -14,12 +14,21 @@ if __name__ == '__main__':
     session=sessionmaker(bind=engine)
     session=session()
 
+teacher_parent= Table(
+    "teacher_parent",
+    Base.metadata,
+    Column("teacher_id", ForeignKey("teachers.id"),primary_key=True),
+    Column("parent_id", ForeignKey("parents.id"),primary_key=True),
+    extend_existing=True,
+)
+
 class Teacher(Base):
     __tablename__='teachers'
     id = Column(Integer(),primary_key=True)
     first_name=Column(String())
     last_name=Column(String())
-    
+    students=relationship('Student',backref=backref("teacher"))
+    parents=relationship("Parent", secondary="teacher_parent",back_populates="teachers")
 
     def __repr__(self):
         return f'Teacher: {self.first_name}'
@@ -29,6 +38,8 @@ class Parent(Base):
     id= Column(Integer(),primary_key=True)
     first_name=Column(String())
     last_name=Column(String())
+    students=relationship("Student",backref=backref("parent"))
+    teachers=relationship("Parent",secondary="teacher_parent",back_populates="parents")
 
 
     def __repr__(self):
@@ -41,7 +52,18 @@ class Student(Base):
     last_name=Column(String())
     class_in= Column(Integer())
     age_in=Column(Integer())
-
+    parent_id=Column(Integer(), ForeignKey("parents.id"))
+    teacher_id=Column(Integer(),ForeignKey("teachers.id"))
+    
+    def __repr__(self):
+        return f'Student(id={self.id}),'+ \
+            f'first_name={self.first_name},'+ \
+            f'last_name = {self.last_name},'+ \
+            f'class_in={self.class_in},'+ \
+            f'age_in={self.age_in},'+ \
+            f'parent_id={self.parent_id},'+ \
+            f'teacher_id={self.teacher_id},'
+            
 
 
 
